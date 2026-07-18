15f5fa0f1c751f9953d7385b4aa75824d726420d
codingwithchitra777 <codingwithchitra@gmail.com>
2026-07-18 22:43:23 +0700

    feat(frontend): add Market filter to History page

--- frontend/src/app/pages/history/history.ts
+++ frontend/src/app/pages/history/history.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './history.html'
 })
 export class HistoryComponent implements OnInit {
@@ -13,19 +14,36 @@ export class HistoryComponent implements OnInit {
 
   readonly trades = signal<any[]>([]);
   readonly loadingTrades = signal<boolean>(false);
+  
+  readonly tickersList = signal<any[]>([]);
+  filterTicker = '';
 
   ngOnInit() {
+    this.loadTickers();
     this.loadTrades();
+  }
+
+  loadTickers() {
+    this.apiService.getPrices().subscribe({
+      next: (data) => {
+        this.tickersList.set(data);
+      }
+    });
   }
 
   loadTrades() {
     this.loadingTrades.set(true);
-    this.apiService.getTrades().subscribe({
+    const tickerParam = this.filterTicker ? this.filterTicker : undefined;
+    this.apiService.getTrades(tickerParam).subscribe({
       next: (data) => {
         this.trades.set(data);
         this.loadingTrades.set(false);
       },
       error: () => this.loadingTrades.set(false)
     });
+  }
+  
+  onFilterChange() {
+    this.loadTrades();
   }
 }
